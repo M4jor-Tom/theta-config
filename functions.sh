7137e33b585cd2d6f960fcddb72826cda1739a82
Tom <tom@fake.mail>
2025-09-08 22:01:35 +0200

    feat(functions.sh): add spread_dashboard

--- functions.sh
+++ functions.sh
@@ -7,3 +7,17 @@ function identify_image() {
 		echo $(basename $1)_$(sha1sum $1 | cut -d ' ' -f 1)_$(uuidgen)
 	fi
 }
+
+function spread_dashboard() {
+	if [[ $# -ne 1 ]]; then
+		echo "USAGE: spread_dashboard <contents-dir>"
+	else
+		for sub_dir in $1/*; do
+			if [[ -d $sub_dir ]]; then
+				sub_dir_name="$(basename $sub_dir)"
+				sub_dir_files_count="$(ls -1 $sub_dir | wc -l)"
+				echo -e "$sub_dir_name\t $sub_dir_files_count"
+			fi
+		done
+	fi
+}
