ec9c05dd2b5195fb1b838c57c92621c64178b703
Tom <tom@fake.mail>
2025-09-10 23:43:12 +0200

    fix(functions.sh): spread_dashboard counts true

--- functions.sh
+++ functions.sh
@@ -15,7 +15,7 @@ function spread_dashboard() {
 		for sub_dir in $1/*; do
 			if [[ -d $sub_dir ]]; then
 				sub_dir_name="$(basename $sub_dir)"
-				sub_dir_files_count="$(ls -1 $sub_dir | wc -l)"
+				let "sub_dir_files_count=$(ls -1 $sub_dir | wc -l) - 1"
 				echo -e "$sub_dir_name\t $sub_dir_files_count"
 			fi
 		done
