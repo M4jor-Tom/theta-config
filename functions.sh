f5a15160b6f2f27f6a6a2603e0e9efd248344226
Tom <tom@fake.mail>
2025-09-11 00:07:02 +0200

    fix(functions.sh): remove identify_image

--- functions.sh
+++ functions.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-function identify_image() {
-	if [[ $# -ne 1 ]]; then
-		echo "USAGE: identify_image <prompt-path>"
-	else
-		echo $(basename $1)_$(sha1sum $1 | cut -d ' ' -f 1)_$(uuidgen)
-	fi
-}
-
 function spread_dashboard() {
 	if [[ $# -ne 1 ]]; then
 		echo "USAGE: spread_dashboard <contents-dir>"
