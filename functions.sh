595057554855f920e5df012781a7995406cc00d2
Tom <tom@fake.mail>
2025-12-23 22:29:03 +0100

    fix(spread_dashboard): arg sanitizing

--- functions.sh
+++ functions.sh
@@ -4,9 +4,9 @@ function spread_dashboard() {
 	if [[ $# -ne 1 ]]; then
 		echo "USAGE: spread_dashboard <contents-dir>"
 	else
-		for sub_dir in $1/*; do
+		for sub_dir in "${1}"/*; do
 			if [[ -d $sub_dir ]]; then
-				sub_dir_name="$(basename $sub_dir)"
+				sub_dir_name=$(basename "${sub_dir}")
 				let "sub_dir_files_count=$(ls -1 $sub_dir | wc -l) - 1"
 				echo -e "$sub_dir_name\t $sub_dir_files_count"
 			fi
@@ -45,5 +45,3 @@ function quadiff() {
     rm "$OLD_DIFF_PATH" "$NEW_DIFF_PATH"
   fi
 }
-
-
